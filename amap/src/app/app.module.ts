11ad37f90daf9f3519d78880a47f79d365e7aefe
Samuel Kling <samtkling@gmail.com>
2018-01-26 00:09:41 -0500

    Addition of table, table-row, data.service

--- amap/src/app/app.module.ts
+++ amap/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { TableRowComponent } from './table-row/table-row.component';
+import { DataService } from './data.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    TableRowComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
